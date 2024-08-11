6e85e6219af3515d439902c0a8eea33aebeff3d5
rsmillan <rsmillan@up.edu.ph>
2024-08-11 19:30:54 +0800

    Update MPmerge_source.py

--- MPmerge_source.py
+++ MPmerge_source.py
@@ -1,9 +1,9 @@
+from test_suite import test_defying1, test_defying2, test_matchmake, test_delete
 import time
 import os
 import subprocess
 import sys
 import random
-
 
 def clear_screen():
     """Clears the terminal screen, if any"""
@@ -381,12 +381,3 @@ def game_start():
         game_start()
 
 game_start()
-
-#####
-#unit tests!
-#Test 1: When mode is not creative nor survival.
-#Test 2: When column is NOT an integer: set, frozenset, list, tuple.
-#Test 3: When column is nothing, i.e. "".
-#Test 4:
-
-#####
